1c833b1642ed65f1711991fc71a1f00229f219a1
HabibAroua <habibha.aroua82@gmail.com>
2017-01-22 00:42:47 +0100

    Dev Class car and the Controller

---
+++ Car Managment System/Controller/VoitureController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Car_Managment_System.Model;
+using System.Data;
+using System.Data.OleDb;
+namespace Car_Managment_System.Controller
+{
+    public class VoitureController : Crud
+    {
+        OleDbConnection cn = new OleDbConnection(Properties.Settings.Default.ch);
+        OleDbCommand cmd = new OleDbCommand();
+        public VoitureController()
+        {
+
+        }
+
+        public Boolean add(Object o)
+        {
+            Voiture v = (Voiture)o;
+            String reference = v.reference;
+            String marque = v.marque;
+            String carburant = v.carburant;
+            int prixParJour = v.prixParJour;
+            
+            try
+            {
+                string req = string.Format("Begin AjouterVoiture('" + reference + "','" +marque + "','" + carburant + "'," + prixParJour + "); end; ");
+                cmd.Connection = cn;
+                cn.Open();
+                cmd.CommandText = req;
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                return true;
+            }
+            catch(OleDbException)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ Car Managment System/Model/Voiture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Managment_System.Model
+{
+    public class Voiture
+    {
+        public String reference
+        {
+            get;
+            set;
+        }
+        public String marque
+        {
+            get;
+            set;
+        }
+        public String carburant
+        {
+            get;
+            set;
+        }
+        public int prixParJour
+        {
+            set;
+            get;
+        }
+        public int prixTotal
+        {
+            set;
+            get;
+        }
+
+        public Voiture()
+        { }
+
+        public Voiture(String reference, String marque, String carburant, int prixParJour, int prixTotal)
+        {
+            this.reference = reference;
+            this.marque = marque;
+            this.carburant = carburant;
+            this.prixParJour = prixParJour;
+            this.prixTotal = prixTotal;
+        }
+    }
+}
